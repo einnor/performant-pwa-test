57f137ffda3b762c9a4207e00ec16347f9f56060
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-06-13 17:30:08 +0300

    Fetch event and loading rom cache if request entry exists

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,4 +9,11 @@ self.addEventListener('install', event => {
 	);
 });
 
-// self.addEventListener()
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request)
+			.then(response => {
+				return response || fetch(event.request);
+			})
+	);
+});
