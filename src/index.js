f3b16f29fe67373508c90468e5754c330a808c8a
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-06-14 18:01:20 +0300

    Logic to install app

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ if ('serviceWorker' in navigator) {
 		});
 }
 
+// Fetch flights and render
 fetch("https://pluralsight-pwa-scratch.firebaseio.com/flights.json")
 	.then(response => response.json())
 	.then(data => {
@@ -18,3 +19,22 @@ fetch("https://pluralsight-pwa-scratch.firebaseio.com/flights.json")
 		});
 		document.querySelector('#screen-flights').innerHTML = html;
 	});
+
+// Listen for the before install to homescreen 
+let deferredPrompt;
+const button = document.querySelector('#a2hs');
+window.addEventListener('beforeinstallprompt', event => {
+	event.preventDefault();
+	deferredPrompt = event;
+	button.style.display = 'block';
+});
+
+button.addEventListener('click', () => {
+	button.style.display = 'none';
+	deferredPrompt.prompt();
+	deferredPrompt.userChoice().then(result => {
+		console.log(result.outcome);
+		// TODO: Send to analytics
+		deferredPrompt = null;
+	})
+});
