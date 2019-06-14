5dbf7a42f2b09d80ee1ebd27bf7cd08dc7d72458
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-06-14 16:47:16 +0300

    Network-first caching strategy

--- src-serviceWorker.js
+++ src-serviceWorker.js
@@ -1,1 +1,6 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+workbox.routing.registerRoute(
+	/https:\/\/pluralsight\-pwa\-scratch\.firebaseio\.com\/flights\.json/,
+	new workbox.strategies.NetworkFirst()
+);
